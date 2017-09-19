4b8c2968b8611750353ea54934b0ff1cd6a655d0
Thomas Bartscher <thomas.bartscher@widder-software.com>
2017-09-19 22:34:05 +0200

    Implement sequence length comparison operations

--- utility/list.lisp
+++ utility/list.lisp
@@ -40,3 +40,170 @@
 (defmethod tree-map (f (tree cons))
   (cons (tree-map f (car tree))
         (tree-map f (cdr tree))))
+
+(defmethod length= ((seq1 null)
+                    (seq2 null))
+  (declare (ignore seq1 seq2))
+  t)
+
+(defmethod length= ((seq1 null)
+                    (seq2 integer))
+  (declare (ignore seq1))
+  (= seq2 0))
+
+(defmethod length= ((seq1 null)
+                    (seq2 cons))
+  (declare (ignore seq1 seq2))
+  nil)
+
+(defmethod length= ((seq1 null)
+                    (seq2 sequence))
+  (declare (ignore seq1))
+  (= (length seq2)
+     0))
+
+(defmethod length= ((seq1 integer)
+                    (seq2 null))
+  (declare (ignore seq2))
+  (= seq1 0))
+
+(defmethod length= ((seq1 integer)
+                    (seq2 integer))
+  (= seq1 seq2))
+
+(defmethod length= ((seq1 integer)
+                    (seq2 cons))
+  (unless (= seq1 0)
+    (length= (1- seq1)
+             (cdr seq2))))
+
+(defmethod length= ((seq1 integer)
+                    (seq2 sequence))
+  (= seq1 (length seq2)))
+
+(defmethod length= ((seq1 cons)
+                    (seq2 null))
+  (declare (ignore seq1 seq2))
+  nil)
+
+(defmethod length= ((seq1 cons)
+                    (seq2 integer))
+  (unless (= seq2 0)
+    (length= (cdr seq1)
+             (1- seq2))))
+
+(defmethod length= ((seq1 cons)
+                    (seq2 cons))
+  (length= (cdr seq1)
+           (cdr seq2)))
+
+(defmethod length= ((seq1 cons)
+                    (seq2 sequence))
+  (length= seq1 (length seq2)))
+
+(defmethod length= ((seq1 sequence)
+                    (seq2 null))
+  (declare (ignore seq2))
+  (= (length seq1)
+     0))
+
+(defmethod length= ((seq1 sequence)
+                    (seq2 integer))
+  (= (length seq1)
+     seq2))
+
+(defmethod length= ((seq1 sequence)
+                    (seq2 cons))
+  (length= (length seq1)
+           seq2))
+
+(defmethod length= ((seq1 sequence)
+                    (seq2 sequence))
+  (= (length seq1)
+     (length seq2)))
+
+(defmethod length> ((seq1 null)
+                    seq2)
+  (declare (ignore seq1 seq2))
+  nil)
+
+(defmethod length> ((seq1 integer)
+                    (seq2 null))
+  (declare (ignore seq2))
+  (/= seq1 0))
+
+(defmethod length> ((seq1 integer)
+                    (seq2 integer))
+  (> seq1 seq2))
+
+(defmethod length> ((seq1 integer)
+                    (seq2 cons))
+  (unless (= seq1 0)
+    (length> (1- seq1)
+             (cdr seq2))))
+
+(defmethod length> ((seq1 integer)
+                    (seq2 sequence))
+  (> seq1 (length seq2)))
+
+(defmethod length> ((seq1 cons)
+                    (seq2 null))
+  (declare (ignore seq1 seq2))
+  t)
+
+(defmethod length> ((seq1 cons)
+                    (seq2 integer))
+  (or (= seq2 0)
+      (length> (cdr seq1)
+               (1- seq2))))
+
+(defmethod length> ((seq1 cons)
+                    (seq2 cons))
+  (length> (cdr seq1)
+           (cdr seq2)))
+
+(defmethod length> ((seq1 cons)
+                    (seq2 sequence))
+  (length> seq1 (length seq2)))
+
+(defmethod length> ((seq1 sequence)
+                    (seq2 null))
+  (declare (ignore seq2))
+  (/= (length seq1)
+      0))
+
+(defmethod length> ((seq1 sequence)
+                    (seq2 integer))
+  (> (length seq1)
+     seq2))
+
+(defmethod length> ((seq1 sequence)
+                    (seq2 cons))
+  (length> (length seq1)
+           seq2))
+
+(defmethod length> ((seq1 sequence)
+                    (seq2 sequence))
+  (> (length seq1)
+     (length seq2)))
+
+(defun prefix? (prefix sequence &key (test #'equal?))
+  (if (length= prefix 0)
+      t
+      (and (length> sequence 0)
+           (@ test (elt prefix 0)
+                   (elt sequence 0))
+           (prefix? (subseq prefix 1)
+                    (subseq sequence 1)
+                    :test test))))
+
+(defun postfix? (postfix sequence &key (test #'equal?))
+  (prefix? (reverse postfix)
+           (reverse sequence)
+           :test test))
+
+(defun intro (n sequence)
+  (subseq sequence 0 (min n (length sequence))))
+
+(defun outro (n sequence)
+  (reverse (intro n (reverse sequence))))
