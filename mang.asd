f0bb2b792155f7ef1dc450a6887602d8a0f33a7a
Thomas Bartscher <thomas.bartscher@widder-software.com>
2017-11-10 23:12:38 +0100

    Start implementation of sound changes

--- mang.asd
+++ mang.asd
@@ -47,7 +47,7 @@
      (:module
       "history"
       :components
-      (
+      ((:file "sound-change")
        ))
      (:module
       "presentation"
---
+++ source/history/sound-change.lisp
@@ -0,0 +1,29 @@
+(in-package #:mang)
+
+(defclass sound-change ()
+  ((%source :type function
+            :reader source<-
+            :initarg :source
+            :initform (create-scanner `(:sequence)))
+   (%target :type (or cons null)
+            :reader target<-
+            :initarg :target
+            :initform nil)
+   (%pre :type function
+         :reader pre<-
+         :initarg :pre
+         :initform (create-scanner `(:sequence)))
+   (%post :type function
+          :reader post<-
+          :initarg :post
+          :initform (create-scanner `(:sequence)))))
+
+;;;; ANNOTATED-STRING<-WORD is necessary(?) to be able tu use CL-PPCRE – it only
+;;;; works on strings. This means that "#" has to be an illegal character in any
+;;;; given glyph.
+(defmethod annotated-string<-word ((word cons))
+  (format nil "~{~A~}"
+          (intersperse "#" word)))
+
+(defmethod annotated-string<-word ((word word))
+  (annotated-string<-word (form<- word)))
