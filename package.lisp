ea9263c92485430a67410976cabcd844f669787a
Thomas Bartscher <thomas.bartscher@widder-software.com>
2019-08-12 18:55:22 +0200

    Some corrections to the package definition

--- package.lisp
+++ package.lisp
@@ -1,11 +1,11 @@
 ;;;; package.lisp
 
 (defpackage #:mang
-  (:use #:cl #:cl-adt #:cells #:equal #:cl-ppcre #:split-sequence)
+  (:use #:cl #:cl-adt #:cells #:equal #:split-sequence)
   (:shadow #:// #:some)
   (:shadowing-import-from
    #:fset
-   #:compare #:with #:less #:convert :set #:union #:@ #:filter #:image #:map
+   #:compare #:with #:less #:convert #:set #:union #:@ #:filter #:image #:map
    #:empty-set #:domain #:reduce #:empty-map #:range #:size #:set-difference-2
    #:map-difference-2 #:arb #:empty? #:complement #:lookup #:intersection
    #:set-difference #:map-union #:$ #:do-map #:do-set #:map-default
