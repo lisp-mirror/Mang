fb063fb93cb2e386a5b198f76834627b91249567
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2019-08-24 12:05:16 +0200

    Implement basic block parsers

--- source/load/blocks.lisp
+++ source/load/blocks.lisp
@@ -3,9 +3,57 @@
 (defun parse-section (name parser)
   (declare (type string name)
            (type function parser))
-  (>> (>> (parse-whitespace)
-          (parse-constant "##")
-          (parse-whitespace-no-newline)
-          (parse-constant name)
-          (parse-expression-end))
+  (>> (parse-whitespace)
+      (parse-constant "##")
+      (parse-whitespace-no-newline)
+      (parse-constant name)
+      (parse-expression-end)
       parser))
+
+(defun parse-subsection (name parser)
+  (declare (type string name)
+           (type function parser))
+  (>> (parse-whitespace)
+      (parse-constant "#")
+      (parse-whitespace-no-newline)
+      (parse-constant name)
+      (parse-expression-end)
+      parser))
+
+(defun parse-repeated-lines (parser &optional (d "")
+                                      (f (lambda (a b)
+                                           (concatenate 'string
+                                                        a b))))
+  (many (>>!
+          _ (parse-whitespace)
+          result parser
+          _ (parse-expression-end))
+        d f))
+
+(defun parse-separated (parser separator &optional (d "")
+                                           (f (lambda (a b)
+                                                (concatenate 'string
+                                                             a b))))
+  (declare (type function parser separator f))
+  (>>!
+    first parser
+    rest (many (>> (parse-whitespace)
+                   separator
+                   (parse-whitespace)
+                   parser)
+               d f)
+    (succeed (funcall f first rest))))
+
+(defun parse-separated-no-newline (parser separator &optional (d "")
+                                                      (f (lambda (a b)
+                                                           (concatenate 'string
+                                                                        a b))))
+  (declare (type function parser separator f))
+  (>>!
+    first parser
+    rest (many (>> (parse-whitespace-no-newline)
+                   separator
+                   (parse-whitespace-no-newline)
+                   parser)
+               d f)
+    (succeed (funcall f first rest))))
