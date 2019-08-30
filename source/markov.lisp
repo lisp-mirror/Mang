e339c3309f6326a4699b3815edf3881b83468640
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2019-08-30 22:29:01 +0200

    Fix PARSE-SIMPLE-MARKOV-SPEC to not filter out phonemes from the learned outro

--- source/markov.lisp
+++ source/markov.lisp
@@ -1,13 +1,13 @@
 (in-package #:mang)
 
 ;;;; A markov spec returns a function taking one argument (which is an initial
-;;;; part of a word) which returns three values. If the word part shouldn't be
-;;;; learned by the markov chain specified by the spec, the three values should
-;;;; be NIL. If the markov spec can learn something from the given word part,
-;;;; the second return value is the outro to be learned, the third value T and
-;;;; the first value a predicate. This predicate should succeed on intros which
-;;;; should be able to receive the outro and fail otherwise. The predicate
-;;;; should also be memoized.
+;;;; part of a word) which returns two values. If the word part shouldn't be
+;;;; learned by the markov chain specified by the spec, the two values should be
+;;;; NIL. If the markov spec can learn something from the given word part, the
+;;;; first return value is the outro to be learned and the second value a
+;;;; predicate. This predicate should succeed on intros which should be able to
+;;;; receive the outro and fail otherwise. The predicate should also be
+;;;; memoized.
 (with-memoization
   (defun parse-simple-markov-spec (glyphs categories)
     (declare (type map categories))
@@ -30,15 +30,19 @@
         (assert (> match-length 0))
         (succeed (set
                   (lambda (word)
-                    (bind ((word (filter filter word)))
-                      (if (length>= word match-length)
-                          (bind ((in+out (subseq (reverse word)
-                                                 0 match-length))
-                                 (intro (reverse (subseq in+out outro)))
-                                 (outro (reverse (subseq in+out 0 outro))))
-                            (values
-                             (memoized (intro filter)
-                                       (lambda (word)
-                                         (postfix? intro (filter filter word))))
-                             outro t))
-                          (values nil nil nil))))))))))
+                    (if (length>= word match-length)
+                        (bind ((rev-word (reverse word))
+                               (rev-init (filter filter
+                                                 (subseq rev-word outro))))
+                          (if (length>= rev-init intro)
+                              (bind ((intro (reverse (subseq rev-init 0 intro)))
+                                     (outro (reverse (subseq rev-word 0
+                                                             outro))))
+                                (values outro
+                                        (memoized (intro filter)
+                                                  (lambda (word)
+                                                    (postfix? intro
+                                                              (filter filter
+                                                                      word))))))
+                              (values nil nil)))
+                        (values nil nil)))))))))
