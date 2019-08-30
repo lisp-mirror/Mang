6f822d0d83a9d2a6b3e4e2b6969d9be83da89902
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2019-08-30 23:33:13 +0200

    Add katz backoff to simple markov spec, remove "anything markov" (which is equivalent to simple markov with intro 0)

--- source/markov.lisp
+++ source/markov.lisp
@@ -1,17 +1,26 @@
 (in-package #:mang)
 
+;;;; everything := 3:1~C 1000> 2:1~C 1000> 1:1~C 1000> 1:1 500> 0:1,
+;;;;               2:1~V 1000> 1:1~V 1000> 1:1 500> 0:1
+
 ;;;; A markov spec returns a function taking one argument (which is an initial
 ;;;; part of a word) which returns two values. If the word part shouldn't be
 ;;;; learned by the markov chain specified by the spec, the two values should be
 ;;;; NIL. If the markov spec can learn something from the given word part, the
-;;;; first return value is the outro to be learned and the second value a
-;;;; predicate. This predicate should succeed on intros which should be able to
-;;;; receive the outro and fail otherwise. The predicate should also be
-;;;; memoized.
+;;;; first return value is the distribution to be learned and the second value a
+;;;; predicate on two values. The first parameter of the predicate is the word
+;;;; generated so far, the second parameter the size of the distribution
+;;;; collected so far from other markov chains in the same run.
 (with-memoization
   (defun parse-simple-markov-spec (glyphs categories)
     (declare (type map categories))
     (>>!
+      katz (<? (>>!
+                 katz (parse-number)
+                 _ (>> (parse-constant ">")
+                       (parse-whitespace-no-newline))
+                 (succeed katz))
+               0)
       intro (parse-number)
       outro (<? (>> (parse-whitespace-no-newline)
                     (parse-constant ":")
@@ -27,7 +36,7 @@
                                  (second cat))))
                  (range glyphs))
       (bind ((match-length (+ intro outro)))
-        (assert (> match-length 0))
+        (assert (> outro 0))
         (succeed (set
                   (lambda (word)
                     (if (length>= word match-length)
@@ -38,23 +47,13 @@
                               (bind ((intro (reverse (subseq rev-init 0 intro)))
                                      (outro (reverse (subseq rev-word 0
                                                              outro))))
-                                (values outro
-                                        (memoized (intro filter)
-                                                  (lambda (word)
-                                                    (postfix? intro
-                                                              (filter filter
-                                                                      word))))))
+                                (values (uniform-distribution (set outro))
+                                        (memoized
+                                         (intro filter)
+                                         (lambda (word dist-size)
+                                           (and (<= dist-size katz)
+                                                (postfix? intro
+                                                          (filter filter
+                                                                  word)))))))
                               (values nil nil)))
                         (values nil nil)))))))))
-
-(defun parse-unconditional-markov-spec ()
-  (>>!
-    _ (>> (parse-constant ".")
-          (parse-whitespace-no-newline))
-    length (<? (parse-number)
-               1)
-    (lambda (word)
-      (if (length>= word length)
-          (values #'true (reverse (subseq (reverse word)
-                                          0 length)))
-          (values nil nil)))))
