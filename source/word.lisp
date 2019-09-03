f469311bb9b478f8ab58cfc66e2bab73edeb79ef
Thomas Bartscher <thomas.bartscher@widder-software.com>
2019-09-03 16:34:07 +0200

    Add PARSE-WORD and PARSE-GLOSS

--- source/word.lisp
+++ source/word.lisp
@@ -1,1 +1,14 @@
 (in-package #:mang)
+
+(defun parse-word (glyphs)
+  (some (parse-glyph glyphs)
+        '() #'cons))
+
+(defun parse-gloss (glyphs)
+  (>>!
+    gloss (parse-identifier *mang-reserved-symbols*)
+    _ (>> (parse-whitespace)
+          (parse-constant ":=")
+          (parse-whitespace))
+    word (parse-word glyphs)
+    (succeed (map (gloss word)))))
