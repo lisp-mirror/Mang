6af98a85d1476d7dc97f7152f13bc2de8bb45437
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2019-09-06 10:17:17 +0200

    Reimplement PARSE-MANG

--- source/read-mang.lisp
+++ source/read-mang.lisp
@@ -6,77 +6,89 @@
       (parse-whitespace-no-newline)
       (parse-identifier)))
 
-(defun parse-mang ()
-  (bind ((languages (empty-map (map (:glyphs (empty-map (empty-set)))
-                                    (:categories (empty-map))
-                                    (:generator (dfsm<- (empty-set)))
-                                    (:markov-spec (empty-map))
-                                    (:store (empty-map))
-                                    (:dictionary (empty-map (empty-map))))))
-         (language nil)
-         (binary-features (empty-set))
-         (valued-features (empty-map (empty-set)))
-         (privative-features (empty-set))
-         (glyphs (empty-map (empty-set)))
-         (categories (empty-map))
-         (generator (dfsm<- (empty-set)))
-         (markov-spec (empty-map))
-         (store (empty-map))
-         (dictionary (empty-map (empty-map))))
-    (many (parser-case
-            (name
-             (parse-language)
-             (when language
-               (setf languages
-                     (with languages language
-                           (map (:glyphs glyphs)
-                                (:categories categories)
-                                (:generator generator)
-                                (:markov-spec markov-spec)
-                                (:store store)
-                                (:dictionary dictionary)))))
-             (bind ((new-language (@ languages name)))
-               (setf glyphs (@ new-language :glyphs)
-                     categories (@ new-language :categories)
-                     generator (@ new-language :generator)
-                     markov-spec (@ new-language :markov-spec)
-                     store (@ new-language :store)
-                     dictionary (@ new-language :dictionary)
-                     language name)))
-            ((binary valued privative)
-             (parse-feature-section)
-             (setf binary-features binary
-                   valued-features valued
-                   privative-features privative))
-            (new-glyphs
-             (parse-glyph-section binary-features valued-features
-                                  privative-features)
-             (assert language)
-             (setf glyphs (map-union glyphs new-glyphs)))
-            (new-categories
-             (parse-category-section glyphs)
-             (assert language)
-             (setf categories (map-union categories new-categories)))
-            (new-generator
-             (// (parse-wordgen-section glyphs categories)
-                 (parse-clustergen-section glyphs categories))
-             (assert language)
-             (setf generator new-generator))
-            ((new-markov-spec new-store)
-             (parse-markov-section (range glyphs)
-                                   categories)
-             (assert language)
-             (setf markov-spec new-markov-spec
-                   store new-store))
-            ((new-dictionary new-store)
-             (parse-dictionary glyphs generator store markov-spec)
-             (assert language)
-             (setf dictionary (map-union dictionary new-dictionary #'map-union)
-                   store new-store)))
-          nil (lambda (a b)
-                (declare (ignore a b))
-                `(,binary-features ,valued-features ,privative-features
-                                   ,languages)))))
+(defun parse-mang (&optional
+                     (languages (empty-map (map (:glyphs
+                                                 (empty-map (empty-set)))
+                                                (:categories
+                                                 (empty-map))
+                                                (:generator
+                                                 (dfsm<- (empty-set)))
+                                                (:markov-spec
+                                                 (empty-map
+                                                  (empty-map
+                                                   (constantly <nodist>))))
+                                                (:store
+                                                 (empty-map
+                                                  (constantly <nodist>)))
+                                                (:dictionary
+                                                 (empty-map (empty-map))))))
+                     (language nil)
+                     (binary-features (empty-set))
+                     (valued-features (empty-map (empty-set)))
+                     (privative-features (empty-set))
+                     (glyphs (empty-map (empty-set)))
+                     (categories (empty-map))
+                     (generator (dfsm<- (empty-set)))
+                     (markov-spec (empty-map (empty-map (constantly <nodist>))))
+                     (store (empty-map (empty-map <nodist>)))
+                     (dictionary (empty-map (empty-map))))
+  (>>!
+    (languages language binary-features valued-features privative-features
+               glyphs categories generator markov-spec store dictionary)
+    (// (<$!> name (parse-language)
+          (bind ((new-language (@ languages name)))
+            `(,(if language
+                   (with languages language
+                         (map (:glyphs glyphs)
+                              (:categories categories)
+                              (:generator generator)
+                              (:markov-spec markov-spec)
+                              (:store store)
+                              (:dictionary dictionary)))
+                   languages)
+               ,name ,binary-features ,valued-features ,privative-features
+               ,(@ new-language :glyphs)
+               ,(@ new-language :categories)
+               ,(@ new-language :generator)
+               ,(@ new-language :markov-spec)
+               ,(@ new-language :store)
+               ,(@ new-language :dictionary))))
+        (<$!> (binary-features valued-features privative-features)
+            (parse-feature-section)
+          `(,languages ,language ,binary-features ,valued-features
+                       ,privative-features ,glyphs ,categories ,generator
+                       ,markov-spec ,store ,dictionary))
+        (<$!> glyphs (parse-glyph-section binary-features valued-features
+                                          privative-features)
+          `(,languages ,language ,binary-features ,valued-features
+                       ,privative-features ,glyphs ,categories ,generator
+                       ,markov-spec ,store ,dictionary))
+        (<$!> categories (parse-category-section glyphs)
+          `(,languages ,language ,binary-features ,valued-features
+                       ,privative-features ,glyphs ,categories ,generator
+                       ,markov-spec ,store ,dictionary))
+        (<$!> generator (// (parse-wordgen-section glyphs categories)
+                            (parse-clustergen-section glyphs categories))
+          `(,languages ,language ,binary-features ,valued-features
+                       ,privative-features ,glyphs ,categories ,generator
+                       ,markov-spec ,store ,dictionary))
+        (<$!> (markov-spec store)
+            (parse-markov-section (range glyphs)
+                                  categories)
+          `(,languages ,language ,binary-features ,valued-features
+                       ,privative-features ,glyphs ,categories ,generator
+                       ,markov-spec ,store ,dictionary))
+        (<$!> (dictionary store)
+            (parse-dictionary glyphs generator store markov-spec)
+          `(,languages ,language ,binary-features ,valued-features
+                       ,privative-features ,glyphs ,categories ,generator
+                       ,markov-spec ,store ,dictionary)))
+    (languages _ binary-features valued-features privative-features _ _ _ _ _ _)
+    (parse-mang languages language binary-features valued-features
+                privative-features glyphs categories generator markov-spec store
+                dictionary)
+    (succeed `(,binary-features ,valued-features ,privative-features
+                                ,languages))))
 
 (defun read-mang-files (file &rest files)
   (apply #'load-by-parser*
