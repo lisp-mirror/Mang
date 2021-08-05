665719d4485b2e9770abc06307fc472e1c0afc36
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2021-08-05 15:40:04 +0200

    Remove build files
    This should be demoted to the status of a library – let other systems implement a buildable application.

--- compile-mang.lisp
+++
@@ -1,9 +0,0 @@
-(load "./mang.asd")
-
-(ql:quickload "mang")
-
-(save-lisp-and-die "mang"
-                   :toplevel #'mang::mang
-                   :executable t
-                   :purify t
-                   :compression 9)
