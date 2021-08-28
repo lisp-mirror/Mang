2f51f362d407b2ce91001acf9f572fefc23bc80e
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2021-08-28 10:01:06 +0200

    Add SIMPLEST-MANG

--- source/read-mang.lisp
+++ source/read-mang.lisp
@@ -1,24 +1,65 @@
 (in-package #:mang)
 
 (defun read-mang-files (feature-file language-file
-                        &optional
-                          dictionary-file)
+                        &key
+                          dictionary-file
+                          diachrony-file)
   (bind (((:values binary-features valued-features privative-features)
           (load-feature-file feature-file))
          (language nil))
     (with-open-file (stream language-file)
-      (bind (((:values loaded bus success?)
-              (parser-call (parse-language-file binary-features valued-features
-                                                privative-features)
+      (values (parser-call (<$~> (parse-language-file binary-features
+                                                      valued-features
+                                                      privative-features)
+                                 (lambda (l)
+                                   (setf language l))
+                                 (lambda (error)
+                                   (return-from read-mang-files
+                                     `(:language-file-failure ,error))))
                            stream)))
-        (if success?
-            (setf language loaded)
-            (return-from read-mang-files
-              `(:language-file-failure ,loaded ,bus)))))
-    (if dictionary-file
-        (with-open-file (stream dictionary-file)
-          (values (parser-call (<~> (parse-dictionary-file language)
-                                    (lambda (error)
-                                      `(:dictionary-file-failure ,error)))
-                               stream)))
-        language)))
+    (when dictionary-file
+      (with-open-file (stream dictionary-file)
+        (values (parser-call (<$~> (parse-dictionary-file language)
+                                   (lambda (l)
+                                     (setf language l))
+                                   (lambda (error)
+                                     (return-from read-mang-files
+                                       `(:dictionary-file-failure ,error))))
+                             stream))))
+    (when diachrony-file
+      (with-open-file (stream diachrony-file)
+        (values (parser-call (<$~> (parse-diachrony-file binary-features
+                                                         valued-features
+                                                         privative-features
+                                                         language)
+                                   (lambda (l)
+                                     (setf language l))
+                                   (lambda (error)
+                                     (return-from read-mang-files
+                                       `(:diachrony-file-error ,error))))
+                             stream))))
+    language))
+
+(defun simplest-mang (feature-file language-file dictionary-file
+                      &key
+                        diachrony-file
+                        computer-readable?)
+  (bind ((language (read-mang-files feature-file language-file
+                                    :dictionary-file dictionary-file
+                                    :diachrony-file diachrony-file)))
+    (if (listp language)
+        (bind (((error-type error)
+                language))
+          (format *error-output* (ecase error-type
+                                   (:language-file-failure
+                                    "Language file failed to load: ~A")
+                                   (:dictionary-file-failure
+                                    "Dictionary file failed to load: ~A")
+                                   (:diachrony-file-error
+                                    "Diachrony file failed to load: ~A"))
+                  error))
+        (loop
+          :while (ask-arbitrary-known-gloss! language)
+          :finally
+             (write-dictionary t language
+                               :computer-readable? computer-readable?)))))
