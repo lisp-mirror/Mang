420bafdedb91c870d6840dcc3633f2fdd2bbb356
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2022-08-06 15:36:36 +0200

    Make sure mang files with duplicated phonemes do not load

--- source/glyphs.lisp
+++ source/glyphs.lisp
@@ -1,28 +1,39 @@
 (in-package #:mang)
 
 (defun parse-glyph-definition (binary-features valued-features
-                               privative-features)
+                               privative-features
+                               &optional
+                                 (glyphs (empty-map (empty-map))))
   (declare (type set binary-features privative-features)
            (type map valued-features))
   (>>!
-    name (parse-identifier *mang-reserved-symbols*)
+    name (parse-identifier (union *mang-reserved-symbols* (domain glyphs)))
     _ (>> (parse-whitespace)
           (parse-constant ":=")
           (parse-whitespace))
     phoneme (parse-feature-set binary-features valued-features
                                privative-features)
-    (succeed (map (name phoneme)
-                  :default (empty-map)))))
+    (bind ((known (origin phoneme glyphs)))
+      (if (empty? known)
+          (succeed (map (name phoneme)
+                        :default (empty-map)))
+          (fail `(:duplicate-phoneme ,phoneme ,name ,known))))))
 
 (defun parse-glyph-definitions (binary-features valued-features
-                                privative-features)
+                                privative-features
+                                &optional
+                                  (glyphs (empty-map (empty-map))))
   (declare (type set binary-features privative-features)
            (type map valued-features))
-  (parse-lines (parse-glyph-definition binary-features valued-features
-                                       privative-features)
-               (empty-map (empty-set))
-               (lambda (glyph glyphs)
-                 (map-union glyphs glyph))))
+  (>>!
+    _ (parse-whitespace)
+    glyph (parse-glyph-definition binary-features valued-features
+                                  privative-features glyphs)
+    _ (parse-expression-end)
+    (// (parse-glyph-definitions binary-features valued-features
+                                 privative-features
+                                 (map-union glyphs glyph))
+        (succeed (map-union glyphs glyph)))))
 
 (defun parse-glyph-section (binary-features valued-features privative-features)
   (declare (type set binary-features privative-features)
